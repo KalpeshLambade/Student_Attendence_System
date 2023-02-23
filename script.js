3f976dff294760cfbcd6f44803fb208a7474eadf
KalpeshLambade <kalpeshkishorlambade@gmail.com>
2023-02-23 12:06:41 +0530

    updated

---
+++ script.js
@@ -0,0 +1,54 @@
+function addStudent(){
+    // alert("student added");
+
+    var StudentName = document.getElementById("addingStudent").value;
+    console.log(StudentName , "studentName");
+
+    var StudentList = JSON.parse(localStorage.getItem("StudentList")) || [] ;
+
+    StudentList.push(StudentName);
+
+    localStorage.setItem("StudentList", JSON.stringify(StudentList));
+}
+
+
+function displayStudentList(){
+    var studentList = JSON.parse(localStorage.getItem('StudentList'));
+    console.log(studentList ,'studentList');
+
+    var divFromHtml = document.getElementById('displayingStudentsList');
+    console.log(divFromHtml, 'divFromHtml');
+
+    var student =[];
+
+    for( var i=0; i < studentList.length; i++){
+        student += `<div><p>${studentList[i]}</p></div>`;
+
+    }
+
+    console.log(student,'student');
+
+    divFromHtml.innerHTML =student;
+}
+displayStudentList();
+
+
+function markingAttence(){
+    var studentList = JSON.parse(localStorage.getItem('StudentList'));
+    console.log(studentList ,'studentList');
+
+    var divFromHtml = document.getElementById('markingAttendance');
+    console.log(divFromHtml, 'divFromHtml');
+
+    var student =[];
+
+    for( var i=0; i < studentList.length ; i++){
+        student += `<div><p>${studentList[i]}</p></div>`;
+
+    }
+
+    console.log(student,'student');
+
+    divFromHtml.innerHTML =student;
+}
+markingAttence();
